d078f3102449f4762579f5e1c820f932e80296b6
SingleHe1990 <LargerStudet@gmail.com>
2018-10-08 20:30:24 +0800

    update

--- src/ComputerBuilderApplication.java
+++ src/ComputerBuilderApplication.java
@@ -17,6 +17,7 @@ public class ComputerBuilderApplication extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
+
         ViewLoader.showStage(new ComputerBuilder(), "/view/computerbuilder.fxml", "Guillermo's Computer Store", stage);
-    }
+}
 }
